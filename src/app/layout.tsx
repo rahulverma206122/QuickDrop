79be5215120a11fc98e384f5f1f945d4c74173d5
rahulverma206122 <rahulverma206122@gmail.com>
2025-09-17 17:27:11 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-h-screen bg-gray-50">
+        <main className="min-h-screen bg-gray-200">
           {children}
         </main>
       </body>
